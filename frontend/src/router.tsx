7d6cf44e3de4c852c75728b7b4a8a4cc460f27b4
osvaldo camacho <valdcam87@gmail.com>
2026-06-02 13:55:43 -0600

    feat: initialize frontend ecosystem with tailwind v4, react-router, and path aliases" -m "This commit establishes the entire core infrastructure for the client side of MonkFlow: * Project Initialization: Scaffolding via Vite + TypeScript and clean up of template boilerplate. * Styling Engine: Modern integration of Tailwind CSS v4 using the native Vite compiler plugin and CSS directives. * Routing Architecture: Implementation of React Router Dom with a decoupled declarative router and an initial layout structure using Outlet. * Developer Experience: Configuration of path aliases (@/*) in tsconfig.app.json and vite.config.ts for cleaner absolute imports."

---
+++ frontend/src/router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AppLayout from "./layouts/AppLayout"
+import DashboardView from "./views/DashboardView"
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AppLayout />} />
+                <Route path="/" element={<DashboardView/>} index />
+            </Routes>
+        </BrowserRouter>
+    )
+}
